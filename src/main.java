b4247598ef5b629d8bc85cf40dff249cbae503f5
licoris <klepehin.k.a@gmail.com>
2026-01-12 15:03:42 +0300

    initial commit

---
+++ src/main.java
@@ -0,0 +1,15 @@
+public class main {
+    public static void main(String[] args) {
+
+        int money = 100;
+        int x = 1100;
+        int percent;
+        if (x > 1000) {
+            percent = 1;
+        } else {
+            percent = 0;
+        }
+        int cash = x / 100 * percent + money + x;
+        System.out.println(cash);
+    }
+}
