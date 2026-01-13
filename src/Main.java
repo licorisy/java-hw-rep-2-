3d9647fb433b807d28a6195111109f7f38c83ecf
licoris <klepehin.k.a@gmail.com>
2026-01-13 16:05:13 +0300

    replenishment

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@ public class Main {
     public static void main(String[] args) {
 
         int money = 100;
-        int x = 1100;
+        int replenishment = 1100;
         int percent;
-        if (x > 1000) {
+        if (replenishment > 1000) {
             percent = 1;
         } else {
             percent = 0;
         }
-        int cash = x / 100 * percent + money + x;
+        int cash = replenishment / 100 * percent + money + replenishment;
         System.out.println(cash);
     }
 }
